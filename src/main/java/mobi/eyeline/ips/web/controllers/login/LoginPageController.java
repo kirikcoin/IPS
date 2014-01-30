9f5cc7449dbf7b77551e69a407180862f6d17b5e
nedenzel <>
2014-01-30 08:16:37 +0000

    Language settings on login page

--- src/main/java/mobi/eyeline/ips/web/controllers/login/LoginPageController.java
+++ src/main/java/mobi/eyeline/ips/web/controllers/login/LoginPageController.java
@@ -2,6 +2,7 @@ package mobi.eyeline.ips.web.controllers.login;
 
 import mobi.eyeline.ips.exceptions.LoginException;
 import mobi.eyeline.ips.service.Services;
+import mobi.eyeline.ips.web.controllers.BaseController;
 
 import javax.faces.context.FacesContext;
 import java.util.Map;
@@ -10,8 +11,7 @@ import java.util.Map;
  * author: Denis Enenko
  * date: 20.01.2014
  */
-public class LoginPageController
-{
+public class LoginPageController extends BaseController {
   private boolean error;
 
   public LoginPageController() {
