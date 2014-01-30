50cc94c2ae6673606679e3ea6c5f0454347ba36a
nedenzel <>
2014-01-30 06:29:28 +0000

    Recovery password

--- src/main/webapp/resources/pages/passwrecovery.js
+++ src/main/webapp/resources/pages/passwrecovery.js
@@ -1,20 +1,16 @@
 var page = {
 
-    login: function () {
-        var l = $('#recoveryEmail');
-        l.val($.trim(l.val()));
-        document.getElementById('rpForm').submit();
-    },
-
     init: function () {
-        var loginFunc = this.login;
-        $('#recoveryEmail').keypress(function (e) {
-            if (e.which == 10 || e.which == 13) //Enter pressed
-                loginFunc();
+        var $emailInput = $('#recoveryEmail');
+        $emailInput.keypress(function (e) {
+            if (e.which == 10 || e.which == 13) {
+                $('a[id$=sendButton]').click();
+                return false;
+            }
+            return true;
         });
-        $('#recoveryEmail').focus();
+
+        $emailInput.focus();
     }
-
-
 
 };
