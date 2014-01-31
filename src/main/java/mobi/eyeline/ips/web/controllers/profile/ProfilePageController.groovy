49761fb841423c789278713d82a4a63c3e7d4de6
nedenzel <>
2014-01-31 06:25:57 +0000

    Continue  profile page

--- src/main/java/mobi/eyeline/ips/web/controllers/profile/ProfilePageController.groovy
+++ src/main/java/mobi/eyeline/ips/web/controllers/profile/ProfilePageController.groovy
@@ -1,14 +1,17 @@
 package mobi.eyeline.ips.web.controllers.profile
 
+import mobi.eyeline.ips.model.User
 import mobi.eyeline.ips.repository.UserRepository
 import mobi.eyeline.ips.service.Services
 import mobi.eyeline.ips.web.controllers.BaseController
 
 class ProfilePageController extends BaseController {
-    private User;
+    private User user;
+    private String userName;
     private UserRepository userRepository = Services.instance().userRepository;
 
     public String getUserFio() {
+        userRepository
         return null;
 
     }
