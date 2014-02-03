fa72a69cb9e67b1b2cbe9623a3293462496cd2e2
nedenzel <>
2014-02-03 08:28:54 +0000

    Profile page

--- src/main/java/mobi/eyeline/ips/web/controllers/profile/ProfilePageController.groovy
+++ src/main/java/mobi/eyeline/ips/web/controllers/profile/ProfilePageController.groovy
@@ -3,14 +3,24 @@ package mobi.eyeline.ips.web.controllers.profile
 import mobi.eyeline.ips.model.User
 import mobi.eyeline.ips.repository.UserRepository
 import mobi.eyeline.ips.service.Services
+import mobi.eyeline.ips.util.HashUtils
 import mobi.eyeline.ips.web.controllers.BaseController
+import org.apache.commons.lang3.StringUtils
+
+import static org.apache.commons.lang3.StringUtils.equalsIgnoreCase
 
 class ProfilePageController extends BaseController {
+    private final UserRepository userRepository = Services.instance().userRepository;
     private User user;
     private String userName;
     def String userFio;
     def String userEmail;
-    private UserRepository userRepository = Services.instance().userRepository;
+
+    def String currentPassword;
+    def String newPassword;
+    def String passwordForConfirm;
+    def boolean error;
+    def boolean success;
 
     ProfilePageController() {
         userName = getUserName();
@@ -19,10 +29,33 @@ class ProfilePageController extends BaseController {
         userEmail = user.email;
     }
 
-    public void saveProfile() {
-        user.email = userEmail;
-        user.fullName = userFio;
-        userRepository.update(user);
+    public String saveProfile() {
+        if(currentPassword==null && newPassword==null && passwordForConfirm==null){
+            user.email = userEmail;
+            user.fullName = userFio;
+            userRepository.update(user);
+            success=true;
+        } else {
+            if(currentPassword!= null && newPassword!=null && passwordForConfirm!=null){
+                String hashedCurrentPassword=HashUtils.hashPassword(currentPassword);
+
+                if (equalsIgnoreCase(hashedCurrentPassword, user.password)) {
+                    if(newPassword==passwordForConfirm){
+                        user.password=HashUtils.hashPassword(newPassword);
+                        userRepository.update(user);
+                        success=true;
+                    } else {
+                        error=true;
+                    }
+                } else {
+                    error=true;
+                }
+            } else {
+                error=true;
+            }
+
+        }
+
     }
 
 }
