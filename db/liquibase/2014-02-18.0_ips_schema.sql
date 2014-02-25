59f5f6989ed68ffa1d7c386e1369ff39e95c8d94
andy <>
2014-02-25 10:08:50 +0000

    Syntax fix.

--- db/liquibase/2014-02-18.0_ips_schema.sql
+++ db/liquibase/2014-02-18.0_ips_schema.sql
@@ -2,5 +2,5 @@
 
 -- changeset andy:8
 
-ALTER TABLE `survey_stats` DROP 'answeredCount';
-ALTER TABLE `survey_stats` DROP 'registeredCount';
+ALTER TABLE `survey_stats` DROP `answeredCount`;
+ALTER TABLE `survey_stats` DROP `registeredCount`;
