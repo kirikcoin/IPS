cd6a8638267e0eac99beca9f064db65056759321
andy <>
2014-02-25 06:25:45 +0000

    Login/password reset form: border radius, `enter' in login field (smaq-62).

--- src/main/webapp/resources/pages/login.js
+++ src/main/webapp/resources/pages/login.js
@@ -1,20 +1,17 @@
 var page = {
 
+  init: function () {
+    $('#j_password').keypress(function (e) {
+      if (e.which == 10 || e.which == 13) //Enter pressed
+        page.login();
+    });
+    $('#j_username').focus();
+  },
+
   login: function () {
     var l = $('#j_username');
     l.val($.trim(l.val()));
     document.getElementById('opForm').submit();
-  },
-
-  init: function () {
-    var loginFunc = this.login;
-    $('#j_password,#j_username').keypress(function (e) {
-      if (e.which == 10 || e.which == 13) //Enter pressed
-        loginFunc();
-    });
-    $('#j_username').focus();
   }
-
-
 
 };
