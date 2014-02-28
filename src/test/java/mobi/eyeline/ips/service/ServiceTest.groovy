a359898678a51e3525ab638087ea58eaeeeb56f5
andy <>
2014-02-28 07:25:37 +0000

    Test for services re-initialization

---
+++ src/test/java/mobi/eyeline/ips/service/ServiceTest.groovy
@@ -0,0 +1,29 @@
+package mobi.eyeline.ips.service
+
+import groovy.mock.interceptor.MockFor
+import mobi.eyeline.ips.properties.Config
+
+class ServiceTest extends GroovyTestCase {
+
+    void testDoubleInitialization() {
+        def newConfig = {
+            def configClass = new MockFor(Config)
+            configClass.demand.getDatabaseProperties() { new Properties() }
+            configClass.demand.getSmtpHost() { '' }
+            configClass.demand.getSmtpPort() { 0 }
+            configClass.demand.getSmtpUsername() { '' }
+            configClass.demand.getSmtpPassword() { '' }
+            configClass.demand.getMailFrom() { '' }
+            configClass.demand.getLoginUrl() { '' }
+            configClass.demand.getSadsMaxSessions() { 1 }
+
+            configClass.proxyDelegateInstance() as Config
+        }
+
+        Services.initialize(newConfig())
+
+        shouldFail(AssertionError) {
+            Services.initialize(newConfig())
+        }
+    }
+}
