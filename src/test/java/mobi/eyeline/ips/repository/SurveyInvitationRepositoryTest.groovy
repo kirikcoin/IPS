1baa1d47807a4e3be649d41e9f5420f922d37468
andy <>
2014-03-05 09:19:30 +0000

    Minor code cleanups in tests.

--- src/test/java/mobi/eyeline/ips/repository/SurveyInvitationRepositoryTest.groovy
+++ src/test/java/mobi/eyeline/ips/repository/SurveyInvitationRepositoryTest.groovy
@@ -2,13 +2,14 @@ package mobi.eyeline.ips.repository
 
 import mobi.eyeline.ips.model.Survey
 import mobi.eyeline.ips.model.SurveyInvitation
+
 import static org.junit.Assert.assertEquals
 
 class SurveyInvitationRepositoryTest extends DbTestCase {
     private SurveyRepository surveyRepository
     private SurveyInvitationRepository surveyInvitationRepository
 
-    def survey1, survey2, survey3, survey4
+    Survey survey1, survey2, survey3, survey4
 
     void setUp() {
         super.setUp()
@@ -16,17 +17,20 @@ class SurveyInvitationRepositoryTest extends DbTestCase {
         surveyInvitationRepository = new SurveyInvitationRepository(db)
     }
 
+    def assertIds =
+            { expected, invitations -> assertEquals(expected, invitations.collect { it.id }) }
+
     void testList1() {
         fillTestData()
-        def assertIds = { expected, invitations -> assertEquals(expected, invitations.collect { it.id }) }
 
-        assertIds([6, 5], surveyInvitationRepository.list(survey3,'value',false,Integer.MAX_VALUE, 0))
-        assertIds([4, 3, 2], surveyInvitationRepository.list(survey2,'value',false,Integer.MAX_VALUE, 0))
+        assertIds([6, 5],
+                surveyInvitationRepository.list(survey3, 'value', false, Integer.MAX_VALUE, 0))
+        assertIds([4, 3, 2],
+                surveyInvitationRepository.list(survey2, 'value', false, Integer.MAX_VALUE, 0))
     }
 
     void testList2() {
         fillTestData()
-        def assertIds = { expected, invitations -> assertEquals(expected, invitations.collect { it.id }) }
 
         assertIds([5,6], surveyInvitationRepository.list(survey3))
     }
@@ -37,24 +41,24 @@ class SurveyInvitationRepositoryTest extends DbTestCase {
     }
 
     private void fillTestData() {
-        survey1 =
-                new Survey(id: 1, startDate: new Date(), endDate: new Date())
-        survey2 =
-                new Survey(id: 2, startDate: new Date(), endDate: new Date())
-        survey3 =
-                new Survey(id: 3, startDate: new Date(), endDate: new Date())
-        survey4 =
-                new Survey(id: 4, active: false, startDate: new Date(), endDate: new Date())
+        [
+            survey1 = new Survey(id: 1),
+            survey2 = new Survey(id: 2),
+            survey3 = new Survey(id: 3),
+            survey4 = new Survey(id: 4, active: false)
+        ].each { s ->
+            s.startDate = new Date()
+            s.endDate = new Date()
+            surveyRepository.save s
+        }
 
-        def si1 = new SurveyInvitation(id: 1, survey: survey1, date: new Date(), value: 1)
-        def si2 = new SurveyInvitation(id: 2, survey: survey2, date: new Date(), value: 2)
-        def si3 = new SurveyInvitation(id: 3, survey: survey2, date: new Date(), value: 3)
-        def si4 = new SurveyInvitation(id: 4, survey: survey2, date: new Date(), value: 4)
-        def si5 = new SurveyInvitation(id: 5, survey: survey3, date: new Date(), value: 5)
-        def si6 = new SurveyInvitation(id: 6, survey: survey3, date: new Date(), value: 6)
-
-        [survey1, survey2, survey3, survey4].each {s ->  surveyRepository.save s}
-        [si1, si2, si3, si4, si5, si6].each {s -> surveyInvitationRepository.save s}
-
+        [
+            new SurveyInvitation(id: 1, survey: survey1, date: new Date(), value: 1),
+            new SurveyInvitation(id: 2, survey: survey2, date: new Date(), value: 2),
+            new SurveyInvitation(id: 3, survey: survey2, date: new Date(), value: 3),
+            new SurveyInvitation(id: 4, survey: survey2, date: new Date(), value: 4),
+            new SurveyInvitation(id: 5, survey: survey3, date: new Date(), value: 5),
+            new SurveyInvitation(id: 6, survey: survey3, date: new Date(), value: 6)
+        ].each {s -> surveyInvitationRepository.save s}
     }
 }
--- src/test/java/mobi/eyeline/ips/repository/SurveyRepositoryTest.groovy
+++ src/test/java/mobi/eyeline/ips/repository/SurveyRepositoryTest.groovy
@@ -12,7 +12,7 @@ class SurveyRepositoryTest extends DbTestCase {
     private QuestionOptionRepository questionOptionRepository
     private UserRepository userRepository
 
-    def user1, user2, user3, user4, user5
+    User user1, user2, user3, user4
 
     void setUp() {
         super.setUp()
@@ -152,75 +152,74 @@ class SurveyRepositoryTest extends DbTestCase {
 
         def assertIds = { expected, surveys -> assertEquals(expected, surveys.collect { it.id }) }
 
-        assertIds([1, 2, 3, 4], surveyRepository.list(null,'', null, null,false,Integer.MAX_VALUE, 0))
-        assertIds([1, 2, 4], surveyRepository.list(null,'A', null, null,false,Integer.MAX_VALUE, 0))
-        assertIds([1, 4, 2], surveyRepository.list(null,'A', null, 'title',true,Integer.MAX_VALUE, 0))
-        assertIds([4, 2, 1], surveyRepository.list(null,'A', null, 'id',false,Integer.MAX_VALUE, 0))
-        assertIds([3, 2, 1], surveyRepository.list(null,'', true, 'accessNumber',false,Integer.MAX_VALUE, 0))
+        def list = surveyRepository.&list
 
-        assertIds([1, 3], surveyRepository.list(null,'F', true, null,false,Integer.MAX_VALUE, 0))
-        assertIds([1, 2, 3], surveyRepository.list(null,'7', true, null,false,Integer.MAX_VALUE, 0))
-        assertIds([3], surveyRepository.list(null,'07', true, null,false,Integer.MAX_VALUE, 0))
-        assertIds([2], surveyRepository.list(user2,'', true, null,false,Integer.MAX_VALUE, 0))
+        assertIds([1, 2, 3, 4], list(null, '', null, null, false, Integer.MAX_VALUE, 0))
+        assertIds([1, 2, 4], list(null, 'A', null, null, false, Integer.MAX_VALUE, 0))
+        assertIds([1, 4, 2], list(null, 'A', null, 'title', true, Integer.MAX_VALUE, 0))
+        assertIds([4, 2, 1], list(null, 'A', null, 'id', false, Integer.MAX_VALUE, 0))
+        assertIds([3, 2, 1], list(null, '', true, 'accessNumber', false, Integer.MAX_VALUE, 0))
 
+        assertIds([1, 3], list(null, 'F', true, null, false, Integer.MAX_VALUE, 0))
+        assertIds([1, 2, 3], list(null, '7', true, null, false, Integer.MAX_VALUE, 0))
+        assertIds([3], list(null, '07', true, null, false, Integer.MAX_VALUE, 0))
+        assertIds([2], list(user2, '', true, null, false, Integer.MAX_VALUE, 0))
     }
 
     void testCount() {
         fillTestData();
 
-        assertEquals(3, surveyRepository.count(null,'A',null))
-        assertEquals(3, surveyRepository.count(null,'F',null))
-        assertEquals(4, surveyRepository.count(null,'7913',null))
+        assertEquals(3, surveyRepository.count(null, 'A', null))
+        assertEquals(3, surveyRepository.count(null, 'F', null))
+        assertEquals(4, surveyRepository.count(null, '7913', null))
 
-        assertEquals(2, surveyRepository.count(null,'A',true))
-        assertEquals(3, surveyRepository.count(null,'7913',true))
-        assertEquals(1, surveyRepository.count(null,'4',null))
-        assertEquals(1, surveyRepository.count(user1,'',null))
-
+        assertEquals(2, surveyRepository.count(null, 'A', true))
+        assertEquals(3, surveyRepository.count(null, '7913', true))
+        assertEquals(1, surveyRepository.count(null, '4', null))
+        assertEquals(1, surveyRepository.count(user1, '', null))
     }
-
 
     private void fillTestData() {
         [
-                user1 = new User(
-                        login: "user1", fullName: "F B", password: "123".pw(), email: "mail@mail.ru", role: Role.CLIENT),
-                user2 = new User(
-                        login: "user2", fullName: "D C", password: "123".pw(), email: "mail2@mail.ru", role: Role.CLIENT),
-                user3 = new User(
-                        login: "user3", fullName: "D F", password: "123".pw(), email: "mail3@mail.ru", role: Role.CLIENT),
-                user4 = new User(
-                        login: "user4", fullName: "F C", password: "123".pw(), email: "mail4@mail.ru", role: Role.CLIENT)
-        ].each {u -> userRepository.save u}
+            user1 = new User(login: 'user1', fullName: 'F B', email: 'mail@mail.ru'),
+            user2 = new User(login: 'user2', fullName: 'D C', email: 'mail2@mail.ru'),
+            user3 = new User(login: 'user3', fullName: 'D F', email: 'mail3@mail.ru'),
+            user4 = new User(login: 'user4', fullName: 'F C', email: 'mail4@mail.ru')
+        ].each { u ->
+            u.role = Role.CLIENT
+            u.password = '123'.pw()
+            userRepository.save u
+        }
 
-        def survey1 =
-                new Survey(id: 1, startDate: new Date(), endDate: new Date(), client: user1)
-        def survey2 =
-                new Survey(id: 2, startDate: new Date(), endDate: new Date(), client: user2)
-        def survey3 =
-                new Survey(id: 3, startDate: new Date(), endDate: new Date(), client: user3)
-        def survey4 =
-                new Survey(id: 4, active: false, startDate: new Date(), endDate: new Date(), client: user4)
+        [
+            new Survey(id: 1, client: user1).with {
+                details = new SurveyDetails(survey: it, title: 'A A')
+                statistics = new SurveyStats(survey: it, accessNumber: "79130000005")
+                it
+            },
 
-        def details1 = new SurveyDetails(survey: survey1, title: 'A A')
-        def details2 = new SurveyDetails(survey: survey2, title: 'B A')
-        def details3 = new SurveyDetails(survey: survey3, title: 'D C')
-        def details4 = new SurveyDetails(survey: survey4, title: 'A C')
+            new Survey(id: 2, client: user2).with {
+                details = new SurveyDetails(survey: it, title: 'B A')
+                statistics = new SurveyStats(survey: it, accessNumber: "79130000006")
+                it
+            },
 
-        def stats1 = new SurveyStats(survey: survey1, accessNumber: "79130000005")
-        def stats2 = new SurveyStats(survey: survey2, accessNumber: "79130000006")
-        def stats3 = new SurveyStats(survey: survey3, accessNumber: "79130000007")
-        def stats4 = new SurveyStats(survey: survey4, accessNumber: "79130000008")
+            new Survey(id: 3, client: user3).with {
+                details = new SurveyDetails(survey: it, title: 'D C')
+                statistics = new SurveyStats(survey: it, accessNumber: "79130000007")
+                it
+            },
 
-        survey1.with {details = details1
-                        statistics = stats1}
-        survey2.with {details = details2
-                        statistics = stats2}
-        survey3.with {details = details3
-                        statistics = stats3}
-        survey4.with {details = details4
-                        statistics = stats4}
-
-        [survey1, survey2, survey3, survey4].each {s-> surveyRepository.save s}
+            new Survey(id: 4, client: user4, active: false, ).with {
+                details = new SurveyDetails(survey: it, title: 'A C')
+                statistics = new SurveyStats(survey: it, accessNumber: "79130000008")
+                it
+            }
+        ].each { s ->
+            s.startDate = new Date()
+            s.endDate = new Date()
+            surveyRepository.save s 
+        }
     }
 
 }
