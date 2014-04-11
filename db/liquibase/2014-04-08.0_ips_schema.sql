729a4ba2de7b857a9923b9a87baf871715536635
artem <>
2014-04-11 03:45:02 +0000

    Fix script format

--- db/liquibase/2014-04-08.0_ips_schema.sql
+++ db/liquibase/2014-04-08.0_ips_schema.sql
@@ -1,6 +1,6 @@
- -- changeset andy:3
+-- liquibase formatted SQL
+-- changeset andy:3
 
-ALTER TABLE `users` ADD COLUMN `only_own_surveys` BOOLEAN DEFAULT FALSE;
-
-ALTER TABLE `surveys` ADD COLUMN `owner_id` INT(11) NULL;
-ALTER TABLE `surveys` ADD CONSTRAINT FOREIGN KEY (`owner_id`) REFERENCES `users` (`id`);
+ALTER TABLE users ADD only_own_surveys BOOLEAN DEFAULT FALSE;
+ALTER TABLE surveys ADD owner_id int(11) default NULL;
+ALTER TABLE surveys ADD FOREIGN KEY surveys_owner_id (owner_id) REFERENCES users (id);
