03a211b304bc66faf79befade6dd398810271cfb
andy <>
2014-05-30 09:59:01 +0000

    Past cleanups in test fill script

--- src/utils/java/mobi/eyeline/ips/repository/FillStats.groovy
+++ src/utils/java/mobi/eyeline/ips/repository/FillStats.groovy
@@ -1,15 +1,14 @@
 package mobi.eyeline.ips.repository
 
 import groovy.sql.Sql
-import groovyx.gpars.GParsPool
 
 sid = 31537
-count = 100_000
-threads = 2
+count = 2_000_000
+parts = 2
 batch = 1000
 
 db = Sql.newInstance(
-        'jdbc:mysql://localhost/ips_migration_test2?useUnicode=true&amp;characterEncoding=UTF-8&rewriteBatchedStatements=true',
+        'jdbc:mysql://localhost/ips_migration_test3?useUnicode=true&characterEncoding=UTF-8&rewriteBatchedStatements=true&allowMultiQueries=true',
         'user',
         'password',
         'com.mysql.jdbc.Driver')
@@ -37,51 +36,14 @@ println "Questions -> options: $questions"
 //
 
 def ranges = { range ->
-    int size = range.size() / threads
-    (0..<threads).collect { range.subList(it * size, (it+1) * size) }
+    int size = range.size() / parts
+    (0..<parts).collect { range.subList it * size, (it+1) * size }
 }(7_999_000_00_00..7_999_000_00_00+(count-1))
 
 
 //
 //  Clean all respondents and answers.
 //
-
-db.withTransaction {
-    db.execute 'delete from answers'
-    db.execute 'delete from respondents'
-}
-
-
-//
-//  Insert respondents and answers.
-//
-
-def toId(i) { i % count as int }
-
-def insertRespondents(range) {
-    db.withTransaction {
-        db.withBatch(batch,
-                """INSERT IGNORE INTO respondents (id, msisdn, survey_id, answer_count, finished)
-                   VALUES (?, ?, ?, ?, ?)""") { ps ->
-            range.each { i -> ps.addBatch toId(i), i as String, sid, questions.size(), 1 }
-        }
-    }
-}
-
-def insertAnswers(range) {
-    db.withTransaction {
-        db.execute 'SET FOREIGN_KEY_CHECKS = 0;'
-        db.withBatch(batch,
-                """INSERT IGNORE INTO answers (respondent_id, question_id, option_id)
-                   VALUES (?, ?, ?)""") { ps ->
-
-            range.each { i ->
-                questions.each { k, v -> ps.addBatch toId(i), k as int, v.first() }
-            }
-        }
-        db.execute 'SET FOREIGN_KEY_CHECKS = 1;'
-    }
-}
 
 def bench = { closure ->
     start = System.currentTimeMillis()
@@ -90,10 +52,52 @@ def bench = { closure ->
 }
 
 def time = bench {
-    GParsPool.withPool(threads) {
-        ranges.eachParallel { insertRespondents(it) }
-        ranges.eachParallel { insertAnswers(it) }
+    db.withTransaction {
+        db.execute '''
+            SET FOREIGN_KEY_CHECKS = 0;
+            TRUNCATE answers;
+            TRUNCATE respondents;
+            SET FOREIGN_KEY_CHECKS = 1;
+        '''
+    }
+}
+println "Cleaning up: $time ms"
+
+
+//
+//  Insert respondents and answers.
+//
+
+def toId(i) { i % count as int }
+
+def inBatch(statement, closure) {
+    db.withTransaction {
+        db.execute 'SET FOREIGN_KEY_CHECKS = 0;'
+        db.execute 'SET UNIQUE_CHECKS = 0;'
+        db.withBatch(batch, statement) { ps -> closure(ps) }
+        db.execute 'SET UNIQUE_CHECKS = 1;'
+        db.execute 'SET FOREIGN_KEY_CHECKS = 1;'
     }
 }
 
-println "$time ms";
+def insertRespondents(range) {
+    nAnswers = questions.size()
+    inBatch("""INSERT IGNORE INTO respondents (id, msisdn, survey_id, answer_count, finished)
+               VALUES (?, ?, ?, ?, ?)""") { ps ->
+        range.each { i -> ps.addBatch toId(i), i as String, sid, nAnswers, 1 }
+    }
+}
+
+def insertAnswers(range) {
+    inBatch("""INSERT IGNORE INTO answers (respondent_id, question_id, option_id)
+               VALUES (?, ?, ?)""") { ps ->
+        range.each { i -> questions.each { k, v -> ps.addBatch toId(i), k as int, v.first() } }
+    }
+}
+
+time = bench {
+    ranges.each { insertRespondents(it) }
+    ranges.each { insertAnswers(it) }
+}
+
+println "Inserting data: $time ms";
