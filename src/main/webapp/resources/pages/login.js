bdf3580db366ba116483097a6958ee64c097579d
andy <andy@ussd.mobi>
2014-07-30 17:35:11 +0700

    Login page, password restore: 1. Fix for <form> with input components 2. Fix for URL params in declarative redirect

--- src/main/webapp/resources/pages/login.js
+++ src/main/webapp/resources/pages/login.js
@@ -1,17 +1,38 @@
 var page = {
 
-  init: function () {
+  /**
+   * @param {string} authUrl Authentication servlet URL.
+   */
+  init: function (authUrl) {
     $('#j_password').keypress(function (e) {
       if (e.which == 10 || e.which == 13) //Enter pressed
-        page.login();
+        page.login(authUrl);
     });
     $('#j_username').focus();
   },
 
-  login: function () {
-    var l = $('#j_username');
-    l.val($.trim(l.val()));
-    document.getElementById('opForm').submit();
+  /**
+   * @param {string} authUrl Authentication servlet URL.
+   */
+  login: function (authUrl) {
+    var $form = $('<form>', {
+      name:     'opForm',
+      method:   'post',
+      action:   authUrl,
+      enctype:  'application/x-www-form-urlencoded'
+    });
+
+    $('<input>', {
+      name: "j_username",
+      value: $.trim($('#j_username').val())
+    }).appendTo($form);
+
+    $('<input>', {
+      name: "j_password",
+      value: $('#j_password').val()
+    }).appendTo($form);
+
+    $form.submit();
   }
 
 };
