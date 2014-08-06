7673d305fd6f417721359fb6e5a2d3bef0a2d648
andy <andy@ussd.mobi>
2014-08-06 19:31:13 +0700

    Changeset updated: dropping `terminal' column.

--- db/liquibase/2014-07-25.0_ips_schema.sql
+++ db/liquibase/2014-07-25.0_ips_schema.sql
@@ -1,5 +1,7 @@
 -- liquibase formatted SQL
 -- changeset nedenzel:11
 
-ALTER TABLE `question_options` ADD COLUMN `next_question` int(11) default null;
-ALTER TABLE `question_options` ADD FOREIGN KEY next_question_id (next_question) REFERENCES questions (id);
+ALTER TABLE `question_options` ADD COLUMN `next_question_id` INT(11) DEFAULT NULL;
+ALTER TABLE `question_options` ADD FOREIGN KEY
+    FK_next_question_id (`next_question`) REFERENCES `questions` (id);
+ALTER TABLE `question_options` DROP COLUMN `terminal`;
