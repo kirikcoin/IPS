9506cf62a4956be306d78d1c619ff8ab27c79783
nedenzel <nedenzel@eyeline.mobi>
2014-10-01 17:12:46 +0700

    ips-265 manager creation script update

--- db/add_user_manager.sql
+++ db/add_user_manager.sql
@@ -11,7 +11,18 @@ set @esdp_login     = 'ipstest';      # `ipstest` - devel, 'ips' - production
 set @esdp_password  = 'password_hash';   # MD5(password)
 
 SELECT SHA2(@password, 256) INTO @pw;
-INSERT INTO users
-  (users_name, email, full_name, `password`, blocked, role, company, phone_number, esdp_provider, esdp_login, esdp_password)
+
+START TRANSACTION;
+
+INSERT INTO ui_profiles
+(id, icon, skin)
 VALUES
-  (@username, @email, @full_name, @pw, 0, 'manager', NULL, NULL, @esdp_provider, @esdp_login, @esdp_password);
+  (null, null, 'MOBAK');
+
+set @ui_profile_id = (SELECT LAST_INSERT_ID());
+
+INSERT INTO users
+  (users_name, email, full_name, `password`, blocked, role, company, phone_number, esdp_provider, esdp_login, esdp_password, ui_profile_id)
+VALUES
+  (@username, @email, @full_name, @pw, 0, 'manager', NULL, NULL, @esdp_provider, @esdp_login, @esdp_password, @ui_profile_id);
+COMMIT;
