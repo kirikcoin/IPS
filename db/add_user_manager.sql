41d9fee87b8a0e140212ba325762e99c2ee67bc3
andy <andy@ussd.mobi>
2014-10-02 16:35:18 +0700

    Minor cleanup in SQL.

--- db/add_user_manager.sql
+++ db/add_user_manager.sql
@@ -14,10 +14,7 @@ SELECT SHA2(@password, 256) INTO @pw;
 
 START TRANSACTION;
 
-INSERT INTO ui_profiles
-(id, icon, skin)
-VALUES
-  (null, null, 'MOBAK');
+INSERT INTO ui_profiles (icon, skin) VALUES (null, 'MOBAK');
 
 set @ui_profile_id = (SELECT LAST_INSERT_ID());
 
@@ -25,4 +22,5 @@ INSERT INTO users
   (users_name, email, full_name, `password`, blocked, role, company, phone_number, esdp_provider, esdp_login, esdp_password, ui_profile_id)
 VALUES
   (@username, @email, @full_name, @pw, 0, 'manager', NULL, NULL, @esdp_provider, @esdp_login, @esdp_password, @ui_profile_id);
+
 COMMIT;
