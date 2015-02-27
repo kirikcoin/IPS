bc0e6083103936ba4ab0f5d7999fc98c38eb186a
nedenzel <nedenzel@eyeline.mobi>
2015-02-27 15:16:05 +0700

    ip-284 little fixes.

--- db/liquibase/2015-02-13.0_ips_schema.sql
+++ db/liquibase/2015-02-13.0_ips_schema.sql
@@ -9,7 +9,7 @@ FOREIGN KEY (`default_question_id`) REFERENCES `questions` (`id`);
 
 ALTER TABLE `questions` ADD `enabled_default_answer` BOOLEAN DEFAULT FALSE;
 
-ALTER TABLE `answers` ADD `type` varchar(255) NOT NULL;
+ALTER TABLE `answers` ADD `type` varchar(255) NOT NULL DEFAULT 'option_answer';
 ALTER TABLE `answers` MODIFY column `option_id` int(11) DEFAULT NULL;
 ALTER TABLE `answers` ADD column `answer_text` varchar(255) DEFAULT NULL;
 
